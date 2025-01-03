0654c79c336b4f82601a7727fa3642a173ce7a3f
SuminJN <wjstnals1211@gmail.com>
2025-01-03 14:47:13 +0900

    .

--- D/D002.java
+++ D/D002.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// https://www.acmicpc.net/problem/10250
+// https://www.acmicpc.net/problem/10250 - D002
 public class D002 {
 
     static int H, W, N, room;
