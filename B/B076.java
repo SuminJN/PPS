9c830096181d23969c2ce00b11cdb0b7eb8ac7cb
SuminJN <wjstnals1211@gmail.com>
2025-01-09 16:50:31 +0900

    25.01.09

---
+++ B/B076.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/17216
+public class B076 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+            }
+
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
